73fce013220d3e60d50cfb647c39203550cce4a2
Lynch00101 <jiricquibrantos870@gmail.com>
2025-03-19 16:41:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "ihhh👉🏻👈🏻🥺";
+  question.innerHTML = "miss u too";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
